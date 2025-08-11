06d0295cce7b517fab33ba6c176c349db137250a
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-08-11 15:19:50 +0100

    Added the header page for the admin dashboard

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import Header from "../components/Navbar";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
